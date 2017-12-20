1f326a0067a8efa6a6b17a1363aa0e4f7e88a85d
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2017-12-20 14:48:14 -0700

    Heroku deploy

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", function(request, response) {
-    response.json({data: crimeData});
+    response.json({data});
 });
 
 app.post("/", function(request, response) {
